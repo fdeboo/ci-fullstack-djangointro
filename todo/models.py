206a1c217a64884ad951ecdfd550126badfd2612
Frances <frances.deboo@gmail.com>
2020-08-07 23:25:11 +0100

    Item model created in models.py and todo_list

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # null=false prevents items from being created without a name programatically
+    # blank=false makes the field 'Required' on forms
+    name = models.CharField(max_length=50, null=False, blank=False)
+    complete = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
