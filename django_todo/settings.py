639d833851a444799d16264a69592ccffd6a9bbc
Frances <frances.deboo@gmail.com>
2020-08-09 16:57:26 +0100

    testing files aded

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://erhyqhsllfvfnq:746f660e1b0efe52770c01e034909b4938922d807169f60cff3ac58b2b1a4285@ec2-35-153-12-59.compute-1.amazonaws.com:5432/ddds49jr0hru79')
 }
 
 
--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     # null=false prevents items from being created without a name programatically
